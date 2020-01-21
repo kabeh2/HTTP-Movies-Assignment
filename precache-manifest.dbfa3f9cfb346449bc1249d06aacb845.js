4dbe580428280462bf16c57b117ee6fdd85cdd12
kabeh2 <mattcnk@hotmail.com>
2020-01-21 12:16:28 -0500

    Updates

---
+++ precache-manifest.dbfa3f9cfb346449bc1249d06aacb845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd17f6d91d3fa9efa60c0a364ed3731b",
+    "url": "/HTTP-Movies-Assignment/index.html"
+  },
+  {
+    "revision": "d917e42073c8273d8999",
+    "url": "/HTTP-Movies-Assignment/static/css/main.11b3bd02.chunk.css"
+  },
+  {
+    "revision": "969ba72765fee98a2a0a",
+    "url": "/HTTP-Movies-Assignment/static/js/2.81871361.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/HTTP-Movies-Assignment/static/js/2.81871361.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d917e42073c8273d8999",
+    "url": "/HTTP-Movies-Assignment/static/js/main.49bf80c9.chunk.js"
+  },
+  {
+    "revision": "c6b54b00c459452af50c",
+    "url": "/HTTP-Movies-Assignment/static/js/runtime-main.7dcb2002.js"
+  }
+]);
